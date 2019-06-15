290340b8c750141e40ee2b235132cd64bd443f0d
Huong <huongnguyenak96@gmail.com>
2019-06-15 18:34:51 +0700

    INNER JOIN page_url

--- models/IndexModel.php
+++ models/IndexModel.php
@@ -8,19 +8,23 @@ class IndexModel extends DBConnect{
     }
     function getFeaturedProduct(){
         // status=1 --> sp dac biet
-        $sql = "SELECT *
-                FROM products 
-                WHERE status=1
-                AND deleted=0
-                ORDER BY id DESC
+        $sql = "SELECT p.*, u.url
+                FROM products p
+                INNER JOIN page_url u
+                ON p.id_url = u.id
+                WHERE p.status=1
+                AND p.deleted=0
+                ORDER BY p.id DESC
                 LIMIT 0,10"; // undeleted
         return $this->getMoreRows($sql);
     }
     function getBestSellerProducts(){
-        $sql = "SELECT p.*, sum(quantity) as tongSL
+        $sql = "SELECT p.*, sum(quantity) as tongSL, u.url
                 FROM products p
                 INNER JOIN bill_detail d
                 ON p.id = d.id_product
+                INNER JOIN page_url u
+                ON p.id_url = u.id
                 WHERE p.deleted=0
                 GROUP BY p.id
                 ORDER BY tongSL DESC
