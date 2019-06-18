b12a3fea793b7e41334885e454ec28f407fe7d46
Huong <huongnguyenak96@gmail.com>
2019-06-18 17:50:05 +0700

    link

--- controllers/DetailController.php
+++ controllers/DetailController.php
@@ -2,6 +2,9 @@
 require_once 'Controller.php';
 class DetailController extends Controller{
     function getDetailPage(){
+        $url = $_GET['link'];
+        echo $url;
+        die;
         $title = 'San pham a';
         return parent::loadView('detail',$title);
     }
