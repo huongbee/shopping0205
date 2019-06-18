4cc286f98ae8be115e5406db1ea9d5a9d04c1895
Huong <huongnguyenak96@gmail.com>
2019-06-18 18:13:58 +0700

    getDetailPage

--- controllers/DetailController.php
+++ controllers/DetailController.php
@@ -1,12 +1,21 @@
 <?php
 require_once 'Controller.php';
+require_once 'models/DetailModel.php';
+
 class DetailController extends Controller{
     function getDetailPage(){
         $url = $_GET['link'];
-        echo $url;
-        die;
-        $title = 'San pham a';
-        return parent::loadView('detail',$title);
+        $model = new DetailModel();
+        $product = $model->getDetailProduct($url);
+        if(!$product){
+            header('Location:404.html');
+            return;
+        }
+        $title = $product->name;
+        $data = [
+            'product'=>$product,
+        ];
+        return parent::loadView('detail',$title, $data);
     }
 }
 
