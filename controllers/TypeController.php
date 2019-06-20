89107cb45e3b0d0e4419fabbd957a5218c603efc
Huong <huongnguyenak96@gmail.com>
2019-06-20 18:14:39 +0700

    type

--- controllers/TypeController.php
+++ controllers/TypeController.php
@@ -1,9 +1,26 @@
 <?php
 require_once 'Controller.php';
+require_once 'models/TypeModel.php';
 
 class TypeController extends Controller{
     static function getTypePage(){
-        return parent::loadView('type','Type page');
+        if(!isset($_GET['link'])){
+            header('Location: 404.html');
+            return;
+        }
+        $url = $_GET['link'];
+        $model = new TypeModel();
+        $type = $model->getTypeByUrl($url);
+        if(!$type){
+            header('Location: 404.html');
+            return;
+        }
+        $products = $model->getProductByType($url);
+        $data = [
+            'products'=> $products,
+            'nameType'=> $type->name
+        ];
+        return parent::loadView('type', $data['nameType'], $data);
     }
 }
 ?>
--- models/TypeModel.php
+++ models/TypeModel.php
@@ -10,6 +10,29 @@ class TypeModel extends DBConnect{
                 WHERE status=1";
         return parent::getMoreRows($sql);
     }
+    function getProductByType(string $urlType){
+        $sql = "SELECT p.*, u.url
+                FROM products p
+                INNER JOIN page_url u
+                ON p.id_url = u.id
+                WHERE p.id_type = (
+                    SELECT c.id
+                    FROM categories c
+                    INNER JOIN page_url pu
+                    ON c.id_url = pu.id
+                    WHERE pu.url = '$urlType'
+                )";
+        return $this->getMoreRows($sql);
+    }
+    function getTypeByUrl($url){
+        $sql = "SELECT c.name
+                FROM categories c
+                INNER JOIN page_url pu
+                ON c.id_url = pu.id
+                WHERE pu.url = '$url'";
+        return $this->getOneRow($sql);
+              
+    }
 }
 
 ?>
