820d3c395de19703e8fc8cb06300e3129afc5e70
Huong <huongnguyenak96@gmail.com>
2019-07-04 18:45:47 +0700

    sql

--- models/CheckoutModel.php
+++ models/CheckoutModel.php
@@ -11,13 +11,17 @@ class CheckoutModel extends DBConnect{
         if($check) return $this->getRecentIdInsert();
         return false;
     }
-    function insertBill($idCustomer, $dateOrder, $total, $promtPrice, $paymentMethod, $note, $token, $tokenDate, $status){
-        $sql = "";
+    function insertBill($idCustomer, $dateOrder, $total, $promtPrice, $paymentMethod, $note, $token, $tokenDate, $status = 0){
+        $sql = "INSERT INTO bills(id_customer, date_order, total, promt_price, payment_method, note, token, token_date, status)
+        VALUES ($idCustomer, '$dateOrder', $total, $promtPrice, '$paymentMethod', '$note', '$token', '$tokenDate', $status)";
+
+        return $this->executeQuery($sql) == true ? $this->getRecentIdInsert() : false;
 
     }
     function insertBillDetail($idBill, $idProduct, $quantity, $price, $discountPrice){
-        $sql = "";
-
+        $sql = "INSERT INTO bill_detail(id_bill, id_product, quantity,price, discount_price)
+        VALUES ($idBill, $idProduct, $quantity, $price, $discountPrice)";
+        return $this->executeQuery($sql);
     }
 }
 
