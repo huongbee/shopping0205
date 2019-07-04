5ecec64e95484dcf162e71443dee6248bc8b5045
Huong <huongnguyenak96@gmail.com>
2019-07-04 19:08:28 +0700

    insertBill

--- controllers/CheckoutController.php
+++ controllers/CheckoutController.php
@@ -1,9 +1,46 @@
 <?php
 require_once 'Controller.php';
+require_once 'models/CheckoutModel.php';
+session_start();
 
 class CheckoutController extends Controller{
     static function getCheckoutPage(){
         return parent::loadView('checkout');
+    }
+    function postCheckout(){
+        if($_POST['name'] =='' || $_POST['email'] == '' || $_POST['gender'] == '' || $_POST['phone'] == '' || $_POST['address'] == '' || $_POST['payment_method']==''){
+            $_SESSION['error'] = 'Vui lòng điền đầy đủ thông tin';
+            header('http://localhost/shopping0205/checkout.php');
+            return;
+        }
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $gender = $_POST['gender'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $paymentMethod = $_POST['payment_method'];
+        $note = $_POST['note'];
+
+        $model = new CheckoutModel();
+        $idCustomer = $model->insertCustomer($name, $gender, $email, $address, $phone);
+        if(!$idCustomer){
+            $_SESSION['error'] = 'Vui lòng thử lại 1';
+            header('http://localhost/shopping0205/checkout.php');
+            return;
+        }
+        $dateOrder = date('Y-m-d', time());
+        $total = '';
+        $promtPrice = '';
+        $token = ''; 
+        $tokenDate = date('Y-m-d H:i:s', time());
+
+        $idBill = $model->insertBill($idCustomer, $dateOrder, $total, $promtPrice, $paymentMethod, $note, $token, $tokenDate, 0);
+        if(!$idBill){
+            $_SESSION['error'] = 'Vui lòng thử lại 2';
+            header('http://localhost/shopping0205/checkout.php');
+            return;
+        }
+        // insert bill_detail
     }
 }
 ?>
